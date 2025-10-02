a7f707894727d3b8f7e345e431816f2657050380
chiragMahajanX <chiragmahajan2385@gmail.com>
2025-10-02 22:22:05 +0530

    check-valid-num

--- initial.py
+++ initial.py
@@ -1,6 +1,36 @@
 import pygame
 import random
 pygame.init()
+
+def is_complete(board):
+    for r in range(9):
+        for c in range(9):
+            if board[r][c] == 0 or not is_valid(board, board[r][c], (r, c)):
+                return False
+    return True
+
+
+def is_valid(board, num, pos):
+    r, c = pos
+    # row check
+    if num in board[r]:
+        return False
+    # col check
+    if num in [board[i][c] for i in range(9)]:
+        return False
+    # 3x3 box check
+    box_x, box_y = c // 3, r // 3
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x*3, box_x*3 + 3):
+            if board[i][j] == num:
+                return False
+    return True
+
+
+
+
+
+
 font = pygame.font.SysFont(None, 48)
 l1=list(range(1,10))
 random.shuffle(l1)
@@ -19,10 +49,10 @@ kkk = [
     [0, 0, 0, 4, 1, 9, 0, 0, 5],
     [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ]
-
+original = [row[:] for row in kkk]
 print(kkk)
 
-scr=pygame.display.set_mode((305,305))
+scr=pygame.display.set_mode((400,400))
 run=True
 white=(255,255,255)
 lc=(0,0,0)
@@ -41,31 +71,22 @@ while run:
             row = pos[1] // cs
             col = pos[0] // cs
             print("Selected cell:", row, col)
-        original = [row[:] for row in kkk]
+
         if i.type == pygame.KEYDOWN:
             if row < 9 and col < 9 and original[row][col]==0:  # valid selection
-                if i.key == pygame.K_1:
-                    kkk[row][col] = 1
-                elif i.key == pygame.K_2:
-                    kkk[row][col] = 2
-                elif i.key == pygame.K_3:
-                    kkk[row][col] = 3
-                elif i.key == pygame.K_4:
-                    kkk[row][col] = 4
-                elif i.key == pygame.K_5:
-                    kkk[row][col] = 5
-                elif i.key == pygame.K_6:
-                    kkk[row][col] = 6
-                elif i.key == pygame.K_7:
-                    kkk[row][col] = 7
-                elif i.key == pygame.K_8:
-                    kkk[row][col] = 8
-                elif i.key == pygame.K_9:
-                    kkk[row][col] = 9
-
-                
-                elif i.key == pygame.K_BACKSPACE:
+                if i.key in [pygame.K_1, pygame.K_2, pygame.K_3,pygame.K_4, pygame.K_5, pygame.K_6,pygame.K_7, pygame.K_8, pygame.K_9]:
+                    number = int(pygame.key.name(i.key)) 
+                     # convert key to int
+                    if is_valid(kkk, number,(row,col)):
+                        kkk[row][col] = number
+                    else:
+                        # if not is_valid(kkk, number, (row, col)):
+                        pygame.draw.rect(scr, (255, 0, 0), [col*33, row*33, 33, 33])
+                elif i.key == pygame.K_BACKSPACE or i.key == pygame.K_DELETE or i.key==pygame.K_0:
                     kkk[row][col] = 0
+
+
+
     pygame.draw.rect(scr,(255,0,0),[col*33,row*33,33,33],3)
 
             
@@ -81,20 +102,25 @@ while run:
         pygame.draw.line(scr, lc, (0, i*cs), (bs, i*cs), lt)
     for i in range(9):
         for j in range(9):
+            if original[i][j] != 0:
+                color = (0, 0, 0)  # fixed puzzle
+            else:
+                color = (0, 0, 255)  # user input
+            
+
 
             if kkk[i][j]!=0:
-                text = font.render(str(kkk[i][j]), True, (0, 0, 0))
+                text = font.render(str(kkk[i][j]), True, color)
                 text_rect = text.get_rect(center=(j*cs + cs//2, i*cs + cs//2))
                 scr.blit(text, text_rect)
 
+    
+    if is_complete(kkk):
+        print("🎉 You solved the puzzle!")
+        run = False
+
         # text_rect = font.get_rect(center=(col*cs + cs//2, row*cs + cs//2))
         # scr.blit(text_rect,font)
-
-
-
-
-
-
 
     pygame.display.update()
 
